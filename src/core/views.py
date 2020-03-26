3a0fb98a60a566b9f46989535355a3efac77ae8b
Hugo Neves de Carvalho <hugonvsc@gmail.com>
2020-03-25 21:50:12 -0300

    Too many things

--- src/core/views.py
+++ src/core/views.py
@@ -2,6 +2,8 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, authenticate, logout
 from django.shortcuts import render, redirect
 
+from django.contrib.auth.models import User
+
 from core.models import Music, Repertory
 
 from core.forms import SignUpForm, LoginForm, NewMusicForm, CreateRepertoryForm
@@ -45,7 +47,11 @@ def logout_view(request):
 
 @login_required
 def profile(request):
-    return render(request, 'account/profile.html')
+    print("oi")
+    user = User.objects.get(username=request.user)
+    repertories = Repertory.objects.filter(owner=user)
+
+    return render(request, 'account/profile.html', {'repertories': repertories})
 
 
 
@@ -79,16 +85,46 @@ def create_repertory(request):
         if form.is_valid():
             name = form.cleaned_data.get('name')
             note = form.cleaned_data.get('note')
-            user
-            
-            Repertory.objects.create(name=name, 
-                                artist=artist,
-                                yt_link=yt_link)
+            user = User.objects.get(username=request.user)
+
+            Repertory.objects.create(owner=user,
+                                   name=name, 
+                                   note=note)
             return redirect('profile')
     else:
         form = CreateRepertoryForm()
 
     return render(request, 'repertory/create_repertory.html', {'form': form})
+
+
+
+@login_required
+def manage_repertory(request, pk):
+    print("PKa", pk)
+    repertory = Repertory.objects.get(pk=pk)
+    musics = repertory.repertory_musics
+    
+    if musics is not None:
+        print("Ma", musics)
+    else:
+        print("M", musics)
+
+    # if request.method == "POST":
+    #     form = CreateRepertoryForm(request.POST)
+        
+    #     if form.is_valid():
+    #         name = form.cleaned_data.get('name')
+    #         note = form.cleaned_data.get('note')
+    #         user = User.objects.get(username=request.user)
+
+    #         Repertory.objects.create(owner=user,
+    #                                name=name, 
+    #                                note=note)
+    #         return redirect('profile')
+    # else:
+    #     form = CreateRepertoryForm()
+
+    return render(request, 'repertory/manage_repertory.html', {'repertory': repertory})
 
 
 
@@ -98,9 +134,6 @@ def add_music_to_repertory(request):
     pass
 
 
-@login_required
-def manage_repertory(request):
-    pass
 
 @login_required
 def list_repertory(request):
--- src/repband/urls.py
+++ src/repband/urls.py
@@ -12,8 +12,8 @@ urlpatterns = [
     path('account/profile', views.profile , name='profile'),
 
 
-    # path('repertory/create', views.create_repertory , name='create_repertory'),
-    # path('repertory/manage', views.manage_repertory , name='manage_repertory'),
+    path('repertory/create', views.create_repertory , name='create_repertory'),
+    path('repertory/manage/<int:pk>', views.manage_repertory , name='manage_repertory'),
     # path('repertory/add_music', views.add_music , name='add_music'),    
 
     path('repertory/add_new_music', views.add_new_music , name='add_new_music'),
